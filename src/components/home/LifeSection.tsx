68db810d4f9b2248f952a4e3619ab88d63f2b20b
johnjoji2006 <johnjoji2006@gmail.com>
2026-09-09 22:21:42 +0530

    chore: Synchronize repository with latest changes

--- src/components/home/LifeSection.tsx
+++ src/components/home/LifeSection.tsx
@@ -1,14 +1,9 @@
-import { TimelineGridTexture } from '../timeline/TimelineGridTexture';
 import { InfrastructurePanel } from './InfrastructurePanel';
 import { TimelineTeaser } from './TimelineTeaser';
 
 export function LifeSection() {
   return (
     <section className="life-section" id="life">
-      <TimelineGridTexture patternId="life-iso-grid" />
-      <div className="life-glow-blob blob-life-left" aria-hidden="true" />
-      <div className="life-glow-blob blob-life-right" aria-hidden="true" />
-
       <div className="life-container">
         <div className="life-grid">
           <div className="life-content">
